6b2945afec9d5ad215a32421c41ef45e35e7e0b4
David Andrs <andrsd@gmail.com>
2025-07-15 06:06:29 -0600

    Use gcov we discovered via find_program

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -101,6 +101,10 @@ if(GMSH2EXO_CODE_COVERAGE)
 
     elseif(CMAKE_C_COMPILER_ID MATCHES "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES "GNU")
         find_program(GCOV_PATH NAMES gcov)
+        if (NOT GCOV_PATH)
+            message(FATAL_ERROR "gcov not found!")
+        endif()
+
         find_program(LCOV_PATH lcov)
         find_program(GENHTML_PATH genhtml)
         mark_as_advanced(FORCE
@@ -126,6 +130,7 @@ if(GMSH2EXO_CODE_COVERAGE)
                 --capture
                 --directory ${PROJECT_BINARY_DIR}
                 --output-file ${COVERAGE_INFO}
+                --gcov-tool ${GCOV_PATH}
                 ${EXCLUDE_REGEX}
         )
 
